52ba17bb83b97a2dd904b3e19f400827b2349040
Lily Pace <lilyfpace@gmail.com>
2014-10-01 14:14:27 -0700

    jquery

--- public/javascript/jquery.js
+++ public/javascript/jquery.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
-  $("#content").click(function () {
+  $("#content").hover(function () {
+    $("#mobilenav").css("left", "-150")
+  });
+
+  $("#menu-icon").hover(function () {
+    $("#mobilenav").css("left", "0")
+  });
+
+  $(".links").click(function () {
     $("#mobilenav").css("left", "-150")
   });
 
